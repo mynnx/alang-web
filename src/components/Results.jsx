080b6c360a110cf0d6f151c15fcaac0256187253
Mark Everett <mark@mynnx.com>
2015-10-24 20:12:11 -0700

    Removing broken import

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Correction from './Correction';
 import {connect} from 'react-redux';
 
 export const Results = React.createClass({
